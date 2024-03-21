516893bc4b1f0457f4655002767bd67a0459704e
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-03-21 14:37:41 +0100

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'reutabla';
+  misCabeceras1: string[] = ["Titulo", "Autor"];
+  misDatos1: any[] = [
+    { "Titulo": "El quijote", "Autor": "Cervantes" },
+    { "Titulo": "100 años de soledad", "Autor": "Gabriel García Márquez" },
+    { "Titulo": "Los pilares de la Tierra", "Autor": "Ken Follet" }
+  ];
+
+  misCabeceras2: string[] = ["Nombre", "Edad", "País"];
+  misDatos2: any[] = [
+    { "Nombre": "Juan", "Edad": 30, "País": "España" },
+    { "Nombre": "Maria", "Edad": 25, "País": "Argentina" },
+    { "Nombre": "Ahmed", "Edad": 35, "País": "Egipto" }
+  ];
+
+  misCabeceras3: string[] = ["Producto", "Precio"];
+  misDatos3: any[] = [
+    { "Producto": "Laptop", "Precio": 800 },
+    { "Producto": "Teléfono", "Precio": 500 },
+    { "Producto": "Tablet", "Precio": 300 }
+  ];
+
+  misCabeceras4: string[] = ["Ciudad", "Población"];
+  misDatos4: any[] = [
+    { "Ciudad": "Nueva York", "Población": 8398748 },
+    { "Ciudad": "Tokio", "Población": 9273000 },
+    { "Ciudad": "Londres", "Población": 8982000 }
+  ];
 }
---
+++ src/app/tabla/tabla.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrl: './tabla.component.css'
+})
+export class TablaComponent {
+  @Input() cabeceras!: string[]
+  @Input() datos!: any[]
+}
